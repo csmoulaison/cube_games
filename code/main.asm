2cebdbaad275f78063392b30bf007a496df25cb3
Conner Moulaison <csmoulaison@gmail.com>
2026-05-23 21:03:46 -0700

    Rip out old intersection code and get ready for fun with Inigo

--- code/main.asm
+++ code/main.asm
@@ -407,75 +407,30 @@ pixel_loop_begin:
     ; Next, we determine whether the ray intersects with an
     ; axis aligned box centered at the origin.
     ;
-    ; Branchless ray/bounding box intersection:
-    ;   (https://tavianator.com/2022/ray_box_boundary.html)
+    ; TODO: Inigo with the clutch:
+    ;   (https://iquilezles.org/articles/intersectors/)
     ;
-    ; tmin/tmax will describe the distance along the ray
-    ; which intersects the box. We will shrink the distance
-    ; between these by clipping them with each plane of the
-    ; box.
+    ; box_size: centered at origin, transform origin and dir accordingly
+    ; vec3 r_inv = 1.0 / r_dir ;
+    ; vec3 n = r_inv * r_origin ;
+    ; vec3 k = abs(r_inv) * box_size
+    ; vec3 t1 = -n - k
+    ; vec3 t2 = -n + k
+    ; float t_near = max(max(t1.x, t1.y), t1.z);
+    ; float t_far = min(min(t2.x, t2.y), t2.z);
     ;
-    ; At the start, tmin=0 and tmax=infinity. If tmin > tmax
-    ; after clipping, the ray does not intersect.
+    ; if(t_near > t_far || t_far < 0.0)
+    ;     intersection = false;
+    ;
+    ; Alright, make sense of this shit.
+    ; normal = (t_near > 0.0) ? step(vec3(t_near), t1)) : step(t2, vec3(t_far)));
+    ; normal *= -sign(r_dir);
+    ; point = vec2(t_near, t_far);
 
-    ; TODO: We are going to try adapting the ray/box 
-    ; intersection algorithm in section 5 of the following
-    ; paper:
-    ;   (https://jcgt.org/published/0007/03/04/paper-lowres.pdf)
-    ; We won't need to reorient the ray or determine the
-    ; winding, as the box is axis aligned and we won't cull
-    ; any faces because it is transparent.
-    
-    movaps  xmm0, xmm11         ; calculate inverse of ray direction
-    movaps  xmm10, dqword [v4_one]
-    divps   xmm10, xmm0         ; xmm10 = 1.0 / dir. apparently, the divide by
-                                ; 0 (=infinity) still works here
-    xorps   xmm9, xmm9          ; xmm9: tmin = 0.0
-    movss   xmm8, [v4_inf]      ; xmm8: tmax = infinity
-    movaps  xmm7, dqword [v4_boxmin] ; xmm7: boxmin
-    movaps  xmm6, dqword [v4_boxmax] ; xmm6: boxmax
-
-macro clip_ray dim {
-    ; available registers: xmm0-xmm5
-    ; t1 = (bmin[d] - origin[d]) * dir_inv[d]
-    extractps esi, xmm7, dim
-    extractps ecx, xmm15, dim
-    extractps edx, xmm10, dim
-    movd    xmm0, esi       ; xmm0: bmin[d]
-    movd    xmm1, ecx       ; xmm1: origin[d]
-    movd    xmm2, edx       ; xmm2: dir inverse[d]
-    subss   xmm0, xmm1      ; xmm0: bmin[d] - origin[d]
-    mulss   xmm0, xmm2
-    movss   xmm3, xmm0      ; xmm3: t1
-
-    ; t2 = (bmin[d] - origin[d]) * dir_inv[d]
-    extractps esi, xmm6, dim
-    movd    xmm0, esi       ; xmm0: bmax[d]
-    subss   xmm0, xmm1      ; xmm0: bmin[d] - origin[d]
-    mulss   xmm0, xmm2      ; xmm0: t2
-
-    ; available registers: xmm1, xmm2, xmm4, xmm5
-    ; tmin = max(tmin, min(min(t1, t2), tmax))
-    movss   xmm5, xmm3      ; xmm5: t1
-    minss   xmm5, xmm0      ; xmm5: min(t1, t2)
-    movss   xmm1, xmm8      ; xmm1: tmax
-    minss   xmm1, xmm5      ; xmm1: min(min(t1, t2), tmax)
-    maxss   xmm9, xmm1      ; xmm9: updated tmin
-    ; tmax = min(tmax, max(max(t1, t2), tmin))
-    movss   xmm5, xmm3      ; xmm5: t1
-    maxss   xmm5, xmm0      ; xmm5: max(t1, t2)
-    movss   xmm1, xmm9      ; xmm1: tmin
-    maxss   xmm1, xmm5      ; xmm1: max(max(t1, t2), tmin)
-    minss   xmm8, xmm1      ; xmm8: updated tmax
-}
-    clip_ray 0
-    clip_ray 1
-    clip_ray 2
 
     ; If we didn't intersect, get color from ray direction
     andps   xmm11, dqword [abs_mask] ; xmm11: absolute value for color
     mulps   xmm11, dqword [v4_255] ; xmm11 scaled by 255 for rgb space
-    ucomiss xmm9, xmm8
     jb      intersect
     jmp     pixel_loop_end
 intersect:
